79c1c19b0632f22abefd9b5147b378cec147bb3e
root <root@ip-172-31-30-85.ec2.internal>
2024-01-08 06:28:43 +0000

    java-file

---
+++ StudentInformation.java
@@ -0,0 +1,21 @@
+public class StudentInformation {
+
+    public static void main(String[] args) {
+        printStudentInfo();
+    }
+
+    private static void printStudentInfo() {
+        String collegeName = "AITS";
+        String studentName = "KRISHNA";
+        int rollNumber = 1596;
+
+        System.out.println("College Name: " + collegeName);
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Roll Number: " + rollNumber);
+
+        System.out.println("\nWelcome, " + studentName + "!");
+        System.out.println("We hope you have a fantastic journey at " + collegeName + ".");  
+        System.out.println("KILL THE SLEEPING PEOPLE");
+    }
+}
+
